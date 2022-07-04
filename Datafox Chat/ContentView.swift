540f63d8d950b5c9ebefc65324493ab0d6566a0a
Datafox-JCP <datafox@outlook.com>
2022-07-04 15:43:45 -0500

    Initial Commit

---
+++ Datafox Chat/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Datafox Chat
+//
+//  Created by Juan Hernandez Pazos on 04/07/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Datafox Chat/Datafox_ChatApp.swift
@@ -0,0 +1,17 @@
+//
+//  Datafox_ChatApp.swift
+//  Datafox Chat
+//
+//  Created by Juan Hernandez Pazos on 04/07/22.
+//
+
+import SwiftUI
+
+@main
+struct Datafox_ChatApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
