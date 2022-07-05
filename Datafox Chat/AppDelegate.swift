7393c41a0e137f3ce671f80c5016cfb1cf321acc
Datafox-JCP <datafox@outlook.com>
2022-07-04 21:06:22 -0500

    - Add Firebase SDK - Add authviewmodel with auth methods

---
+++ Datafox Chat/AppDelegate.swift
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.swift
+//  Datafox Chat
+//
+//  Created by Juan Hernandez Pazos on 04/07/22.
+//
+
+import SwiftUI
+import FirebaseCore
+
+class AppDelegate: NSObject, UIApplicationDelegate {
+  func application(_ application: UIApplication,
+                   didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+    FirebaseApp.configure()
+    return true
+  }
+}
---
+++ Datafox Chat/ViewModels/AuthViewModel.swift
@@ -0,0 +1,24 @@
+//
+//  AuthViewModel.swift
+//  Datafox Chat
+//
+//  Created by Juan Hernandez Pazos on 04/07/22.
+//
+
+import Foundation
+import FirebaseAuth
+
+class AuthViewModel {
+    
+    static func isUserLoggedIn() -> Bool {
+        return Auth.auth().currentUser != nil
+    }
+    
+    static func getLoggedInUserId() -> String {
+        return Auth.auth().currentUser?.uid ?? ""
+    }
+    
+    static func logout() {
+        try? Auth.auth().signOut()
+    }
+}
